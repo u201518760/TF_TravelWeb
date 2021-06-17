d0277252b66808efac0c4c0e304d27a95a685973
CarlosCR2307 <u201816727@upc.edu.pe>
2021-06-17 14:07:47 -0500

    Pruebita

--- Travel_Web/src/main/java/pe/edu/upc/spring/TravelWebApplication.java
+++ Travel_Web/src/main/java/pe/edu/upc/spring/TravelWebApplication.java
@@ -24,15 +24,46 @@ public class TravelWebApplication  implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		String password="123456";
-		String contraseña="contra";
+		String contraseña1="contra1";
+		String contraseña2="contra2";
+		String contraseña3="contra3";
+		String contraseña4="contra4";
+		String contraseña5="contra5";
+		String contraseña6="contra6";
+		String contraseña7="contra7";
 		for(int i=0;i<1;i++) {
 			String bcryptPassword=passwordEncoder.encode(password);
 			System.out.println(bcryptPassword);
 		}
 		for(int i=0;i<1;i++) {
-			String bcryptPassword=passwordEncoder.encode(contraseña);
+			String bcryptPassword=passwordEncoder.encode(contraseña1);
 			System.out.println(bcryptPassword);
 		}
+		for(int i=0;i<1;i++) {
+			String bcryptPassword=passwordEncoder.encode(contraseña2);
+			System.out.println(bcryptPassword);
+		}
+		for(int i=0;i<1;i++) {
+			String bcryptPassword=passwordEncoder.encode(contraseña3);
+			System.out.println(bcryptPassword);
+		}
+		for(int i=0;i<1;i++) {
+			String bcryptPassword=passwordEncoder.encode(contraseña4);
+			System.out.println(bcryptPassword);
+		}
+		for(int i=0;i<1;i++) {
+			String bcryptPassword=passwordEncoder.encode(contraseña5);
+			System.out.println(bcryptPassword);
+		}
+		for(int i=0;i<1;i++) {
+			String bcryptPassword=passwordEncoder.encode(contraseña6);
+			System.out.println(bcryptPassword);
+		}
+		for(int i=0;i<1;i++) {
+			String bcryptPassword=passwordEncoder.encode(contraseña7);
+			System.out.println(bcryptPassword);
+		}
+		
 		
 	}
 
